3a8171bc903368483547e1ea0e76e71ebdd41d18
Chris Leveille <cdleveille@gmail.com>
2023-01-29 18:30:26 -0500

    additionally update xenia executable path and switch emulator steam category strings

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import path from "path";
 interface IParser {
 	configTitle: string;
 	disabled: boolean;
+	executable: {
+		path: string;
+	};
+	steamCategory: string;
 }
 
 const IS_PROD = process.env.NODE_ENV !== "development";
@@ -14,7 +18,7 @@ const fileName = "userConfigurations.json";
 const enabledFileName = "enabledParsers.json";
 
 const folderPathProd = "/home/deck/.config/steam-rom-manager/userData";
-const folderPathDev = "C:/Users/cdlev/Downloads";
+const folderPathDev = "C:/repos/Personal/srm-parser-enable/test";
 const folderPath = IS_PROD ? folderPathProd : folderPathDev;
 
 const enabledFilePath = path.join(folderPath, enabledFileName);
@@ -39,8 +43,15 @@ const readEnabledParserList = () => {
 const updateData = (data: IParser[], enabledParsers: string[]) => {
 	try {
 		return data.map(parser => {
-			const disabled = !enabledParsers.includes(parser.configTitle);
-			return { ...parser, disabled } as IParser;
+			const { configTitle, executable } = parser;
+			const disabled = !enabledParsers.includes(configTitle);
+			if (configTitle === "Microsoft Xbox 360 - Xenia") {
+				executable.path = "/run/media/mmcblk0p1/Emulation/roms/xbox360/xenia_canary.exe";
+			}
+			if (["Nintendo Switch - Yuzu", "Nintendo Switch Ryujinx"].includes(configTitle)) {
+				parser.steamCategory = "${Switch}";
+			}
+			return { ...parser, disabled, executable } as IParser;
 		});
 	} catch (error) {
 		throw `Error updating parser config data: ${error}`;
