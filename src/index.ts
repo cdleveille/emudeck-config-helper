c40bbf8be06c906c524541a00cebaf77121699fc
Chris Leveille <cdleveille@gmail.com>
2023-01-30 02:29:40 -0500

    more verbose output

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,13 @@ import {
 } from "./operations";
 
 try {
+	console.log("*** START - emudeck-config-helper operations starting... ***");
 	const data = readParserConfigData();
 	const enabledParsers = readEnabledParserList();
 	const updatedData = updateConfigFileData(data, enabledParsers);
 	writeUpdatedConfigFileData(updatedData);
+	console.log("*** END - emudeck-config-helper operations completed successfully. ***");
 } catch (error) {
 	console.error(error);
+	console.error("*** ABEND - emudeck-config-helper operations completed with errors. ***");
 }
--- src/operations.ts
+++ src/operations.ts
@@ -12,7 +12,10 @@ const ENABLED_FILE_PATH = path.join(FOLDER_PATH, ENABLED_FILE_NAME);
 
 export const readParserConfigData = () => {
 	try {
-		return JSON.parse(readFileSync(CONFIG_FILE_PATH, { encoding: ENCODING })) as IParser[];
+		console.log(`Reading parser config data from "${CONFIG_FILE_PATH}"...`);
+		const data = JSON.parse(readFileSync(CONFIG_FILE_PATH, { encoding: ENCODING })) as IParser[];
+		console.log("Parser config data read from file successfully.");
+		return data;
 	} catch (error) {
 		throw `Error reading parser config data from "${CONFIG_FILE_PATH}": ${error}`;
 	}
@@ -20,7 +23,10 @@ export const readParserConfigData = () => {
 
 export const readEnabledParserList = () => {
 	try {
-		return JSON.parse(readFileSync(ENABLED_FILE_PATH, { encoding: ENCODING })) as string[];
+		console.log(`Reading enabled parser list from "${ENABLED_FILE_PATH}"...`);
+		const data = JSON.parse(readFileSync(ENABLED_FILE_PATH, { encoding: ENCODING })) as string[];
+		console.log("Enabled parser list read from file successfully.");
+		return data;
 	} catch (error) {
 		throw `Error reading enabled parser list from "${ENABLED_FILE_PATH}": ${error}`;
 	}
@@ -28,7 +34,8 @@ export const readEnabledParserList = () => {
 
 export const updateConfigFileData = (data: IParser[], enabledParsers: string[]) => {
 	try {
-		return data.map(parser => {
+		console.log("Updating parser config file data...");
+		const updatedData = data.map(parser => {
 			const { configTitle, executable } = parser;
 			const disabled = !enabledParsers.includes(configTitle);
 			if (configTitle === Parser.XENIA) {
@@ -39,6 +46,8 @@ export const updateConfigFileData = (data: IParser[], enabledParsers: string[]) 
 			}
 			return { ...parser, disabled, executable } as IParser;
 		});
+		console.log("Parser config file data updated successfully.");
+		return updatedData;
 	} catch (error) {
 		throw `Error updating parser config file data: ${error}`;
 	}
@@ -46,8 +55,10 @@ export const updateConfigFileData = (data: IParser[], enabledParsers: string[]) 
 
 export const writeUpdatedConfigFileData = (updatedData: IParser[]) => {
 	try {
+		console.log(`Writing updated parser config file data to "${CONFIG_FILE_PATH}"...`);
 		writeFileSync(CONFIG_FILE_PATH, JSON.stringify(updatedData, null, 4));
+		console.log("Wrote updated parser config file data successfully.");
 	} catch (error) {
-		throw `Error writing updated config file data to "${CONFIG_FILE_PATH}": ${error}`;
+		throw `Error writing updated parser config file data to "${CONFIG_FILE_PATH}": ${error}`;
 	}
 };
